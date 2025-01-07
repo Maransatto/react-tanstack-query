ab10221f86e14a8ee72c6b6b2a4dce39193b789a
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-06 22:13:07 -0300

    feat: 418. A Challenge! The Solution (an elegant way of doing it)

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,23 +1,16 @@
 import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from "../Header.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import Header from "../Header.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
-  const params = useParams();
-  const id = params.id;
+  const { id } = useParams();
   const navigate = useNavigate();
 
-  const {
-    data: event,
-    isPending,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["event", { eventId: id }],
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { eventId: id }],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
@@ -29,13 +22,68 @@ export default function EventDetails() {
   } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["events", "event"] });
+      queryClient.invalidateQueries({ queryKey: ["events"] });
       navigate("/events");
     },
   });
 
   function handleDelete() {
     mutate({ id });
+  }
+
+  let content;
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "Failed to fetch events."}
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            {isDeletingPending && <span>Deleting...</span>}
+            {!isDeletingPending && (
+              <button onClick={handleDelete}>Delete</button>
+            )}
+
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
@@ -46,48 +94,13 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {isPending && <LoadingIndicator />}
-      {isError && (
-        <ErrorBlock
-          title="An error occurred"
-          message={error.info?.message || "Failed to fetch events."}
-        />
-      )}
+      <article id="event-details">{content}</article>
+
       {isDeletingError && (
         <ErrorBlock
           title="An error occurred"
           message={deletingError.info?.message || "Failed to delete event"}
         />
-      )}
-      {event && (
-        <article id="event-details">
-          <header>
-            <h1>{event.title}</h1>
-            <nav>
-              {isDeletingPending && <span>Deleting...</span>}
-              {!isDeletingPending && (
-                <button onClick={handleDelete}>Delete</button>
-              )}
-
-              <Link to="edit">Edit</Link>
-            </nav>
-          </header>
-          <div id="event-details-content">
-            <img
-              src={`http://localhost:3000/${event.image}`}
-              alt={event.title}
-            />
-            <div id="event-details-info">
-              <div>
-                <p id="event-details-location">EVENT LOCATION</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>
-                  {event.date} at {event.time}
-                </time>
-              </div>
-              <p id="event-details-description">{event.description}</p>
-            </div>
-          </div>
-        </article>
       )}
     </>
   );
