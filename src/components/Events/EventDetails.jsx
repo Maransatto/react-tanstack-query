edc82527450a933fe0b7c548611ff46f611e0ed7
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-06 21:49:04 -0300

    feat: 417. A Challenge! The Problem

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,43 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const params = useParams();
+  const id = params.id;
+  const navigate = useNavigate();
+
+  const {
+    data: event,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["event", { eventId: id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+  });
+
+  const {
+    mutate,
+    isPending: isDeletingPending,
+    isError: isDeletingError,
+    error: deletingError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events", "event"] });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +46,49 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <LoadingIndicator />}
+      {isError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "Failed to fetch events."}
+        />
+      )}
+      {isDeletingError && (
+        <ErrorBlock
+          title="An error occurred"
+          message={deletingError.info?.message || "Failed to delete event"}
+        />
+      )}
+      {event && (
+        <article id="event-details">
+          <header>
+            <h1>{event.title}</h1>
+            <nav>
+              {isDeletingPending && <span>Deleting...</span>}
+              {!isDeletingPending && (
+                <button onClick={handleDelete}>Delete</button>
+              )}
+
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img
+              src={`http://localhost:3000/${event.image}`}
+              alt={event.title}
+            />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">EVENT LOCATION</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {event.date} at {event.time}
+                </time>
+              </div>
+              <p id="event-details-description">{event.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -12,7 +12,7 @@ export default function NewEvent() {
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["events"] });
+      queryClient.invalidateQueries({ queryKey: ["events", "event"] });
       navigate("/events");
     },
   });
